b70d6cef984df6e4a4348502749bf1eec41e3d2e
hitesh-2006 <hiteshkiran2006@gmail.com>
2026-02-12 22:12:33 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,28 @@
+#personilisation option B
+n=int(input("no of inputs :"))
+name=input("enter the name :")
+data=[]
+for i in range(n):
+    a=input("enter data :")
+    data.append(a)
+no_list=[]
+sr_list=[]
+for i in data:
+    if i.isdigit():
+        no_list.append(i)
+    else :
+        sr_list.append(i)
+        
+#personilisation option B    
+x=len(no_list)
+y=len(sr_list)
+x-=1
+y-=1
+if(len(name)%2==0):
+    no_list.remove(no_list[0])
+    sr_list.remove(sr_list[0])
+else:
+    no_list.remove(no_list[x])
+    sr_list.remove(sr_list[y])
+print(no_list)
+print(sr_list)
